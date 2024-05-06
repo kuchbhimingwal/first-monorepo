0b8e8b7c552c89fb8b23085a59cd1c6d6c9ef012
kuchbhimingwal <shubhammingi@gmail.com>
2024-05-06 02:14:07 -0400

    made a page in ui to have access throghout the repo

---
+++ packages/ui/src/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div>
+      <input type="text" placeholder="email"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+    </div>
+  )
+}
+
+export default login
---
+++ packages/ui/src/signup.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function signup() {
+  return (
+    <div>
+      <input type="text" placeholder="name"/>
+      <input type="text" placeholder="email"/>
+      <input type="password" placeholder="password"/>
+      <button>signup</button>
+    </div>
+  )
+}
+
+export default signup
